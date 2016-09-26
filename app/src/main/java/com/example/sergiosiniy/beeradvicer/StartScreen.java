8cb06eab1c1f195038acb8bad82d4e8843d95607
Serhii Synohub <serhii.synohub@gmail.com>
2016-09-26 11:28:08 +0300

    add menu

--- app/src/main/java/com/example/sergiosiniy/beeradvicer/StartScreen.java
+++ app/src/main/java/com/example/sergiosiniy/beeradvicer/StartScreen.java
@@ -3,6 +3,8 @@ package com.example.sergiosiniy.beeradvicer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class StartScreen extends AppCompatActivity {
@@ -29,6 +31,23 @@ public class StartScreen extends AppCompatActivity {
     public void addNewBeer(View view){
         Intent addBeer = new Intent(this, AddNewBeer.class);
         startActivity(addBeer);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.send_beer:
+                Intent addBeer = new Intent(this, AddNewBeer.class);
+                startActivity(addBeer);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void closeTheApp(View view){
