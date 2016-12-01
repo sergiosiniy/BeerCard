b87f764d62ad94b0f521b8543aafccae1cceccbd
Serhii Synohub <serhii.synohub@gmail.com>
2016-12-01 12:54:21 +0200

    checking network state

--- app/src/main/java/com/example/sergiosiniy/beeradvicer/activities/StartScreen.java
+++ app/src/main/java/com/example/sergiosiniy/beeradvicer/activities/StartScreen.java
@@ -1,13 +1,18 @@
 package com.example.sergiosiniy.beeradvicer.activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.sergiosiniy.beeradvicer.R;
+
+import java.net.InetAddress;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -15,6 +20,13 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        if(!isNetworkConnected()){
+            Toast.makeText(this,"No network connection! \nCheck your Wi-Fi connection! ",
+                    Toast.LENGTH_SHORT).show();
+        }else if(!isServerReachable()){
+            Toast.makeText(this,"Server is unreachable! Try again later. \nSory =(",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
@@ -43,7 +55,22 @@ public class StartScreen extends AppCompatActivity {
         }
     }
 
-    public void closeTheApp(View view){
-        System.exit(1);
+    private boolean isNetworkConnected(){
+        final ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo()!=null;
     }
+
+    private boolean isServerReachable(){
+
+        try{
+            InetAddress address = InetAddress.getByName("31.134.121.230");
+            return !address.equals("");
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+
 }
